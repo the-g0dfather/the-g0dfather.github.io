7a52e6979a030ad1ec148c3feabb7cf8511ae5d2
the-g0dfather <75117970+the-g0dfather@users.noreply.github.com>
2020-11-28 21:25:33 +0545

    Updated

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 const testButton = document.getElementById('test');
 
 const config = {
-  birthdate: 'Nov 25, 2020',
+  birthdate: 'Nov 30, 2020',
   name: 'Pratikshya'
 };
 
@@ -509,3 +509,12 @@ x = setInterval(function () {
   //     console.log("happy birthday");
   // }
 }, second);
+console.log(
+  '%c Hey Babe!',
+  'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'
+);
+console.log(
+  '%c Happy Birthdayyy!',
+  'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'
+);
+console.log("Here's your easter egg 92389382");
